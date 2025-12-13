592fb49ca7cabd489c2f099a59e7a19ecdc547ce
SaraEOlivera <saraeolivera@gmail.com>
2025-12-13 19:44:00 -0300

    Ejercicio sobre matriz

--- app.js
+++ app.js
@@ -74,3 +74,28 @@ function aVeryBigSum(ar) {
 unArray = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]  
 
 console.log("aVeryBigSum", aVeryBigSum(unArray));
+
+/* 5 - Given a square matrix, calculate the absolute difference between the sums
+/  of its diagonals. For example, the square matrix arr is shown below:
+ 1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = 1+5+9 = 15
+The right-to-left diagonal = 3+5+9 = 17
+Their absolute difference is 15 - 17 = 2
+*/
+
+function diagonalDifference(arr) {
+    // Write your code here
+    let sumLR = 0;
+    let sumRL = 0;
+    for(i = 0; i< arr.length; i++){
+        sumLR += arr[i][i];
+        sumRL += arr[i][(arr.length - 1)- i];
+    }
+    return Math.abs(sumLR - sumRL); 
+}
+
+matriz = [[1,2,3],[4,5,6],[9,8,9]];
+console.log(diagonalDifference(matriz));
+
